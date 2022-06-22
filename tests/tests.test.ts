6cdc365d573d11e1c12ca02e539003aaed6ef65c
Jay Meistrich <jmeistrich@gmail.com>
2022-06-22 17:38:47 +0800

    Add prettier

--- tests/tests.test.ts
+++ tests/tests.test.ts
@@ -4,41 +4,41 @@ describe('Basic', () => {
     test('Has value', () => {
         const obs = obsProxy(10);
         expect(obs.value).toEqual(10);
-    })
-
-    test("set value", () => {
-      const obs = obsProxy<number>(10);
-      obs.value = 20;
-      expect(obs.value).toEqual(20);
     });
 
-    test("set function", () => {
-      const obs = obsProxy<number>(10);
-      const ret = obs.set(20);
-      expect(obs.value).toEqual(20);
-      expect(ret.value).toEqual(20);
-      expect(ret.set).not.toBeUndefined();
+    test('set value', () => {
+        const obs = obsProxy<number>(10);
+        obs.value = 20;
+        expect(obs.value).toEqual(20);
     });
 
-    test("require set function", () => {
-      const obs = obsProxy<number>(10);
-      const ret = obs.set(20);
-      expect(obs.value).toEqual(20);
-      expect(ret.value).toEqual(20);
-      expect(ret.set).not.toBeUndefined();
+    test('set function', () => {
+        const obs = obsProxy<number>(10);
+        const ret = obs.set(20);
+        expect(obs.value).toEqual(20);
+        expect(ret.value).toEqual(20);
+        expect(ret.set).not.toBeUndefined();
     });
 
-    test("require set function", () => {
-        const consoleErrorMock = jest
-          .spyOn(console, "error")
-          .mockImplementation();
-
-      const obs = obsProxy({ test: 'hi' }, true);
-    //   @ts-ignore
-      obs.test = 'hello';
-
-      expect(consoleErrorMock).toHaveBeenCalled();
-
-      consoleErrorMock.mockRestore();
+    test('require set function', () => {
+        const obs = obsProxy<number>(10);
+        const ret = obs.set(20);
+        expect(obs.value).toEqual(20);
+        expect(ret.value).toEqual(20);
+        expect(ret.set).not.toBeUndefined();
     });
-})
+
+    test('require set function', () => {
+        const consoleErrorMock = jest.spyOn(console, 'error').mockImplementation();
+
+        const obs = obsProxy({ test: 'hi' }, true);
+        //   @ts-ignore
+        obs.test = 'hello';
+
+        expect(consoleErrorMock).toHaveBeenCalled();
+
+        consoleErrorMock.mockRestore();
+    });
+});
+
+describe('Listeners', () => {});
