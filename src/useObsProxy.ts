16ff73f074684123a52758e90dab53efbf34f920
Jay Meistrich <jmeistrich@gmail.com>
2022-06-24 00:53:15 +0800

    Optimize useObsProxy

--- src/useObsProxy.ts
+++ src/useObsProxy.ts
@@ -1,12 +1,11 @@
 import { useForceRender } from '@legendapp/tools';
-import { useCallback, useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { listenToObs } from './ObsProxyFns';
 import { MappedProxyValue, ObsListener, ObsProxy } from './ObsProxyInterfaces';
 import { disposeListener } from './ObsProxyListener';
 
 interface SavedRef {
     args?: ObsProxy[];
-    prevArgs?: ObsProxy[];
     listeners?: ObsListener[];
 }
 
@@ -19,27 +18,17 @@ function useObsProxy<T extends ObsProxy<any>[]>(...args: T): MappedProxyValue<T>
         };
     }
 
-    // Change handler
-    const onChange = useCallback(() => {
-        if (ref.current.listeners.length) {
-            forceRender();
-        }
-    }, []); // eslint-disable-line react-hooks/exhaustive-deps
-
-    // Compare to previous args and update listeners if any changed or first mount
+    const prevArgs = ref.current.args;
     ref.current.args = args;
 
-    const prevArgs = ref.current.prevArgs;
-    if (prevArgs !== args) {
-        doListening(args, prevArgs, ref.current.listeners, onChange);
-        ref.current.prevArgs = args;
-    }
+    // Compare to previous args and update listeners if any changed or first mount
+    updateListeners(args, prevArgs, ref.current.listeners, forceRender);
 
     // Dispose listeners on unmount
     useEffect(
         () => () => {
             if (ref.current.listeners) {
-                ref.current.listeners?.forEach((listener) => listener && disposeListener(listener));
+                ref.current.listeners.forEach(disposeListener);
                 ref.current.listeners = [];
             }
         },
@@ -49,12 +38,12 @@ function useObsProxy<T extends ObsProxy<any>[]>(...args: T): MappedProxyValue<T>
     return args.map((obs) => obs?.value) as MappedProxyValue<T>;
 }
 
-const doListening = (args: ObsProxy[], prevArgs: ObsProxy[], listeners: ObsListener[], onChange: () => void) => {
+const updateListeners = (args: ObsProxy[], prevArgs: ObsProxy[], listeners: ObsListener[], onChange: () => void) => {
     const num = Math.max(args.length, prevArgs ? prevArgs.length : 0);
     for (let i = 0; i < num; i++) {
         const obs = args[i];
-        if (obs && (!prevArgs || obs !== prevArgs[i])) {
-            if (prevArgs?.[i] && listeners[i]) {
+        if (!prevArgs || obs !== prevArgs[i]) {
+            if (listeners[i]) {
                 disposeListener(listeners[i]);
                 listeners[i] = undefined;
             }
