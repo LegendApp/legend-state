49406aaa80dfb908cb503b43db2f059dc99daf78
Jay Meistrich <jmeistrich@gmail.com>
2022-06-29 18:16:31 +0800

    useNewObsProxy can take a function to reduce compute, and returns an array with the raw value

--- src/react/useNewObsProxy.ts
+++ src/react/useNewObsProxy.ts
@@ -1,16 +1,17 @@
+import { isFunction, MemoFnComponentWithRef } from '@legendapp/tools';
 import { useMemo } from 'react';
 import { obsProxy } from '../ObsProxy';
 import { ObsProxy } from '../ObsProxyInterfaces';
 import { useObsProxy } from './useObsProxy';
 
-function useNewObsProxy<T extends object>(value: T, observe?: boolean): ObsProxy<T> {
-    const obs = useMemo(() => obsProxy(value), []); // eslint-disable-line react-hooks/exhaustive-deps
+function useNewObsProxy<T extends object>(value: T | (() => T), observe?: boolean): [ObsProxy<T>, T] {
+    const obs = useMemo(() => obsProxy(isFunction(value) ? value() : value), []); // eslint-disable-line react-hooks/exhaustive-deps
 
     if (observe) {
         useObsProxy(() => [obs]);
     }
 
-    return obs;
+    return [obs, obs.get() as T];
 }
 
 export { useNewObsProxy };
