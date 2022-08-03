228ef26688750cb2cbc5d908bcaf7355f839f886
Jay Meistrich <jmeistrich@gmail.com>
2022-08-04 01:01:41 +0800

    Move persistObservable to a separate export path

--- index.ts
+++ index.ts
@@ -2,4 +2,3 @@ export { shouldRender, mergeIntoObservable, shallow } from './src/helpers';
 export { observable } from './src/observable';
 export { observableBatcher } from './src/observableBatcher';
 export * from './src/observableInterfaces';
-export { persistObservable } from './src/persistObservable';
---
+++ persist.ts
@@ -0,0 +1,1 @@
+export { persistObservable } from './src/persistObservable';
