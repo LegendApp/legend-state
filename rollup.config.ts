c569fd74a44b8d752f2094cf1a94f5f8588ffa5e
Jay Meistrich <jmeistrich@gmail.com>
2022-08-08 22:08:33 +0800

    Change .js files to esm

--- rollup.config.ts
+++ rollup.config.ts
@@ -5,44 +5,48 @@ import typescript from '@rollup/plugin-typescript';
 import pkg from './package.json';
 
 // export default ['./react'].map((exp) => {
-export default Object.keys(pkg.exports).map((exp) => {
-    let f = exp.slice(2);
+export default Object.keys(pkg.exports)
+    .map((exp) => {
+        if (exp.endsWith('json')) return;
 
-    const external = ['react', 'react-native-mmkv', '@legendapp/state', '@legendapp/state/persist'];
+        let f = exp.slice(2);
 
-    if (!f) f = 'index';
+        const external = ['react', 'react-native-mmkv', '@legendapp/state', '@legendapp/state/persist'];
 
-    return {
-        input: './' + f + '.ts',
-        output: [
-            {
-                file: './dist/' + f + '.cjs',
-                format: 'cjs',
-                sourcemap: true,
-            },
-            {
-                file: './dist/' + f + '.js',
-                format: 'cjs',
-                sourcemap: true,
-            },
-            {
-                file: './dist/' + f + '.esm.mjs',
-                format: 'es',
-                sourcemap: true,
-            },
-        ],
-        external: external,
-        plugins: [
-            resolve(),
-            commonjs(),
-            typescript({
-                outputToFilesystem: true,
-                paths: {
-                    react: ['node_modules/react'],
-                    '@legendapp/state': ['./index'],
-                    '@legendapp/state/persist': ['./persist'],
+        if (!f) f = 'index';
+
+        return {
+            input: './' + f + '.ts',
+            output: [
+                {
+                    file: './dist/' + f + '.cjs',
+                    format: 'cjs',
+                    sourcemap: true,
                 },
-            }),
-        ],
-    };
-});
+                {
+                    file: './dist/' + f + '.js',
+                    format: 'es',
+                    sourcemap: true,
+                },
+                {
+                    file: './dist/' + f + '.esm.mjs',
+                    format: 'es',
+                    sourcemap: true,
+                },
+            ],
+            external: external,
+            plugins: [
+                resolve(),
+                commonjs(),
+                typescript({
+                    outputToFilesystem: true,
+                    paths: {
+                        react: ['node_modules/react'],
+                        '@legendapp/state': ['./index'],
+                        '@legendapp/state/persist': ['./persist'],
+                    },
+                }),
+            ],
+        };
+    })
+    .filter((a) => a);
