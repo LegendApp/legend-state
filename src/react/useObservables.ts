f4dbc76810e7508a4d04acc6c3b3efd5d25b1001
Jay Meistrich <jmeistrich@gmail.com>
2022-08-11 18:32:54 +0800

    Add more comments to useObservables

--- src/react/useObservables.ts
+++ src/react/useObservables.ts
@@ -34,6 +34,8 @@ interface SavedRef<T extends ObservableTypeRender | Record<string, ObservableTyp
  * A React hook that listens to observables and returns their values.
  *
  * @param fn A function that returns a single observable, an array of observables, or a flat object of observables
+ * @param deps If present, useObservables will re-activate if the values in the list change. By default it will never reactivate, which is the same as a [] argument.
+
  *
  * @see https://www.legendapp.com/dev/state/react/#useobservables
  */
@@ -52,6 +54,7 @@ export function useObservables<
 }
 
 function setup(ref: RefObject<SavedRef<any>>, forceRender: () => void) {
+    // Clear out previous listeners before computing new ones
     if (ref.current.listeners.size > 0) {
         ref.current.listeners.forEach((dispose) => dispose());
         ref.current.listeners.clear();
@@ -59,6 +62,8 @@ function setup(ref: RefObject<SavedRef<any>>, forceRender: () => void) {
 
     let previousPrimitives: string;
     const updateFromSelector = () => {
+        // Compute returns a string concatenated with all of the primitive values in the arguments
+        // so we can easily compare that to the previous result and re-render only if it's different.
         const args = ref.current.fn();
         const { primitives, value } = compute(args);
         if (previousPrimitives !== primitives) {
@@ -81,19 +86,29 @@ function setup(ref: RefObject<SavedRef<any>>, forceRender: () => void) {
         tracking.is = true;
         tracking.nodes = [];
 
+        // tracking.nodes is updated any time a primitive is accessed or get() is called
+        // so after calling fn(), tracking.nodes will be filled with those selectors
         const args = ref.current.fn();
         const selectorNodes = tracking.nodes;
 
         tracking.nodes = [];
 
+        // compute calls symbolGet on all of the observable arguments so afterwards
+        // tracking.nodes will be filled with all of the observables in the arguments
         const { primitives, value } = compute(args);
 
         tracking.is = false;
+
+        // Set up listeners on all nodes
+        // Selectors only need to check for changes in the primitives while
+        // observables need to check observable arguments to add listeners to any
+        // that weren't there before
         updateListeners(selectorNodes, updateFromSelector);
         updateListeners(tracking.nodes, update);
 
         ref.current.value = value;
 
+        // Re-render when called by a change handler
         if (!ref.current.isFirst) {
             previousPrimitives = primitives;
             forceRender();
@@ -104,37 +119,39 @@ function setup(ref: RefObject<SavedRef<any>>, forceRender: () => void) {
 }
 
 function compute(args) {
-    let ret;
+    let value;
     let primitives: string = '';
 
     if (args !== undefined && args !== null) {
         let isPrim;
         if (isArray(args)) {
-            ret = [];
+            // Process arguments as an array
+            value = [];
             for (let i = 0; i < args.length; i++) {
                 isPrim = isPrimitive(args[i]);
                 if (isPrim) primitives += args[i];
-                ret[i] = isPrim ? args[i] : getObservableRawValue(args[i]);
+                value[i] = isPrim ? args[i] : getObservableRawValue(args[i]);
             }
         } else {
             isPrim = isPrimitive(args);
             if (isPrim || args[symbolIsObservable] || args[symbolShallow]) {
+                // Process a single value
                 if (isPrim) primitives += args;
-
-                ret = isPrim ? args : getObservableRawValue(args as ObservableTypeRender);
+                value = isPrim ? args : getObservableRawValue(args as ObservableTypeRender);
             } else if (isObject(args)) {
-                ret = {};
+                // Process a flat object of observables
+                value = {};
                 const keys = Object.keys(args);
                 const length = keys.length;
                 for (let i = 0; i < length; i++) {
                     const key = keys[i];
                     isPrim = isPrimitive(args[key]);
                     if (isPrim) primitives += args[key];
-                    ret[key] = isPrim ? args[key] : getObservableRawValue(args[key]);
+                    value[key] = isPrim ? args[key] : getObservableRawValue(args[key]);
                 }
             }
         }
     }
 
-    return { value: ret, primitives };
+    return { value: value, primitives };
 }
