9ae5cccad66b68a303f7a7751e14ffafb7c27c9a
Jay Meistrich <jmeistrich@gmail.com>
2022-08-13 00:34:58 +0800

    Remove the not actually working concept of deleting no longer needed listeners

--- src/react/listenWhileCalling.ts
+++ src/react/listenWhileCalling.ts
@@ -1,30 +1,22 @@
-import { tracking, ObservableListenerDispose, TrackingNode, onChangeShallow, onChange } from '@legendapp/state';
+import { ObservableListenerDispose, onChange, onChangeShallow, tracking } from '@legendapp/state';
 
 export function listenWhileCalling<T>(fn: () => T, listeners: Set<ObservableListenerDispose>, updateFn: () => void) {
     const trackingPrev = tracking.nodes;
     tracking.nodes = [];
 
-    let ret = fn();
+    const ret = fn();
 
     const nodes = tracking.nodes;
 
     tracking.nodes = trackingPrev;
 
-    const listenersNotSeen = new Set<ObservableListenerDispose>();
     for (let i = 0; i < nodes.length; i++) {
         const { node, shallow } = nodes[i];
 
         // Listen to this path if not already listening
         if (!node.listeners?.has(updateFn)) {
             listeners.add(shallow ? onChangeShallow(node, updateFn) : onChange(node, updateFn));
-        } else {
-            listenersNotSeen.delete(updateFn);
         }
-    }
-
-    for (let dispose of listenersNotSeen) {
-        listeners.delete(dispose);
-        dispose();
     }
 
     return ret;
