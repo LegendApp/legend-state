f8dbcf47e9865e3095bc0a4a414d37ad623fd950
Jay Meistrich <jmeistrich@gmail.com>
2022-08-12 14:17:10 +0800

    Fix calling reducer directly was a bad idea

--- src/react/useComputed.ts
+++ src/react/useComputed.ts
@@ -2,9 +2,14 @@ import { ObservableListenerDispose } from '@legendapp/state';
 import { useEffect, useReducer, useRef } from 'react';
 import { listenWhileCalling } from './listenWhileCalling';
 
+function useForceRender() {
+    const [, forceRender] = useReducer((s) => s + 1, 0);
+    return () => forceRender();
+}
+
 export function useComputed<T>(selector: () => T): T {
     const ref = useRef<Set<ObservableListenerDispose>>(new Set());
-    const [, forceRender] = useReducer((s) => s + 1, 0);
+    const forceRender = useForceRender();
 
     useEffect(() => () => ref.current.forEach((dispose) => dispose()), []);
     const value = listenWhileCalling(selector, ref.current, forceRender);
