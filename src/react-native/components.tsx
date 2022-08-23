3e76183b15079553f6fe785fcbe8b076f624be07
Jay Meistrich <jmeistrich@gmail.com>
2022-08-23 21:04:29 +0800

    Support className and style as functions taking the current value

--- src/react-native/components.tsx
+++ src/react-native/components.tsx
@@ -1,36 +1,48 @@
 import { createElement, forwardRef, useCallback } from 'react';
+import { isFunction } from '@legendapp/state';
 import {
     NativeSyntheticEvent,
+    StyleProp,
     Switch as RNSwitch,
     SwitchChangeEvent,
     SwitchProps,
     TextInput as RNTextInput,
     TextInputChangeEventData,
     TextInputProps,
+    TextStyle,
+    ViewStyle,
 } from 'react-native';
 import { ObservableChild, Primitive } from '../observableInterfaces';
 import { observer } from '../react/observer';
 
-interface Props<T> {
+interface Props<T, TStyle> {
     bind?: ObservableChild<T>;
+    style?: StyleProp<TStyle> | ((value: T) => StyleProp<TStyle>);
 }
 
-export const Binder = function <TBind extends Primitive, TProps extends { onChange?: any }>(
-    Component,
-    getValue: (p: any) => TBind
-) {
+export const Binder = function <
+    TBind extends Primitive,
+    TStyle,
+    TProps extends { onChange?: any; style?: StyleProp<any> }
+>(Component, getValue: (p: any) => TBind) {
     return observer(
-        forwardRef(function Bound({ bind, onChange, ...props }: Props<TBind> & TProps, ref) {
+        forwardRef(function Bound(
+            { bind, onChange, style, ...props }: Props<TBind, TStyle> & Omit<TProps, 'style'>,
+            ref
+        ) {
             if (bind) {
                 const _onChange = useCallback((e) => {
                     bind.set(getValue(e));
                     onChange?.(e);
                 }, []);
 
-                return createElement(
-                    Component,
-                    Object.assign({}, props, { onChange: _onChange, value: bind.observe(), ref })
-                );
+                const value = bind.observe();
+
+                if (isFunction(style)) {
+                    style = style(value);
+                }
+
+                return createElement(Component, Object.assign({}, props, { onChange: _onChange, value, style, ref }));
             } else {
                 return createElement(Component, ref ? { ...props, ref } : props);
             }
@@ -39,9 +51,9 @@ export const Binder = function <TBind extends Primitive, TProps extends { onChan
 };
 
 export namespace LS {
-    export const TextInput = Binder<Primitive, TextInputProps>(
+    export const TextInput = Binder<Primitive, TextStyle, TextInputProps>(
         RNTextInput,
         (e: NativeSyntheticEvent<TextInputChangeEventData>) => e.nativeEvent.text
     );
-    export const Switch = Binder<Primitive, SwitchProps>(RNSwitch, (e: SwitchChangeEvent) => e.value);
+    export const Switch = Binder<Primitive, ViewStyle, SwitchProps>(RNSwitch, (e: SwitchChangeEvent) => e.value);
 }
--- src/react/components.tsx
+++ src/react/components.tsx
@@ -1,26 +1,49 @@
-import { ChangeEvent, createElement, forwardRef, LegacyRef, useCallback } from 'react';
+import { ChangeEvent, createElement, CSSProperties, forwardRef, LegacyRef, useCallback } from 'react';
+import { isFunction } from '@legendapp/state';
 import { ObservableChild, Primitive } from '../observableInterfaces';
 import { observer } from './observer';
 
 interface Props<T> {
     bind?: ObservableChild<T>;
+    className?: string | ((value: T) => string);
+    style?: CSSProperties | ((value: T) => CSSProperties);
 }
 
-export const Binder = function <TBind extends Primitive, TElement, TProps extends { onChange?: any }>(Component) {
+export const Binder = function <
+    TBind extends Primitive,
+    TElement,
+    TProps extends { onChange?: any; className?: string; style?: CSSProperties }
+>(Component) {
     return observer(
-        forwardRef(function Bound({ bind, onChange, ...props }: Props<TBind> & TProps, ref: LegacyRef<TElement>) {
-            const _onChange = useCallback(
-                (e: ChangeEvent<HTMLInputElement>) => {
-                    bind.set(e.target.value as any);
-                    onChange?.(e);
-                },
-                [onChange]
-            );
+        forwardRef(function Bound(
+            { bind, onChange, className, style, ...props }: Props<TBind> & Omit<TProps, 'className' | 'style'>,
+            ref: LegacyRef<TElement>
+        ) {
+            if (bind) {
+                const _onChange = useCallback(
+                    (e: ChangeEvent<HTMLInputElement>) => {
+                        bind.set(e.target.value as any);
+                        onChange?.(e);
+                    },
+                    [onChange]
+                );
 
-            return createElement(
-                Component,
-                Object.assign({}, props, { onChange: _onChange, value: bind.observe(), ref })
-            );
+                const value = bind.observe();
+
+                if (isFunction(className)) {
+                    className = className(value);
+                }
+                if (isFunction(style)) {
+                    style = style(value);
+                }
+
+                return createElement(
+                    Component,
+                    Object.assign({}, props, { onChange: _onChange, value, className, style, ref })
+                );
+            } else {
+                return createElement(Component, ref ? { ...props, ref } : props);
+            }
         })
     );
 };
