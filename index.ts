991c26c323d5afb78ba6a7edb653f4e4463e8aae
Jay Meistrich <jmeistrich@gmail.com>
2022-09-07 20:55:15 +0800

    Rearrange exports to stop exporting symbols publicly

--- index.ts
+++ index.ts
@@ -1,13 +1,16 @@
-export { symbolDateModified, symbolIsObservable, Tracking } from './src/globals';
+export { Tracking } from './src/globals';
 export { isObservable, mergeIntoObservable } from './src/helpers';
-/** @internal */
-export { isArray, isFunction, isObject, isPrimitive } from './src/is';
 export { observable } from './src/observable';
 export { observableBatcher } from './src/observableBatcher';
 export { observableComputed } from './src/observableComputed';
 export { observableEvent } from './src/observableEvent';
 export * from './src/observableInterfaces';
+
+/** @internal */
+export { isArray, isFunction, isObject, isPrimitive } from './src/is';
 /** @internal */
 export { onChange, onChangeShallow } from './src/on';
 /** @internal */
 export { tracking } from './src/tracking';
+/** @internal */
+export { symbolDateModified, symbolIsObservable } from './src/globals';
