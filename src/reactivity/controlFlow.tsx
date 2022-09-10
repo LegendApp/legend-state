59749b78f1dcad0e7bd9a5408875fed235ba9bb8
Jay Meistrich <jmeistrich@gmail.com>
2022-09-10 22:09:33 +0800

    Change control flow components to use useComputed instead of observer, which should be a bit faster

--- src/reactivity/controlFlow.tsx
+++ src/reactivity/controlFlow.tsx
@@ -1,20 +1,20 @@
 import { isFunction, isObservable, Tracking } from '@legendapp/state';
-import { createElement, ReactElement, ReactNode, useMemo, useRef } from 'react';
-import type { NotPrimitive, ObservableObject, ObservableReadable } from '../observableInterfaces';
-import { observer } from '@legendapp/state/react';
+import { observer, useComputed } from '@legendapp/state/react';
+import { createElement, memo, ReactElement, ReactNode, useMemo, useRef } from 'react';
+import type { NotPrimitive, ObservableObject } from '../observableInterfaces';
 
-export const Isolate = observer(function Isolate({ children }: { children: () => ReactNode }): ReactElement {
-    return (children as () => ReactElement)();
-});
+export function Isolate({ children }: { children: () => ReactNode }): ReactElement {
+    return useComputed(children) as ReactElement;
+}
 
-export const Memo = observer(
+export const Memo = memo(
     function Memo({ children }: { children: () => ReactNode }): ReactElement {
-        return (children as () => ReactElement)();
+        return useComputed(children) as ReactElement;
     },
     () => true
 );
 
-export const Show = observer(function Show<T>({
+export function Show<T>({
     if: if_,
     else: else_,
     children,
@@ -25,32 +25,54 @@ export const Show = observer(function Show<T>({
     memo?: boolean;
     children: () => ReactNode;
 }): ReactElement {
-    if (isFunction(if_)) {
-        if_ = if_();
-    }
+    const show = useComputed(() => {
+        if (isFunction(if_)) {
+            if_ = if_();
+        }
 
-    if (isObservable(if_)) {
-        if_ = (if_ as ObservableObject).get();
-    }
+        if (isObservable(if_)) {
+            if_ = (if_ as ObservableObject).get();
+        }
+        return if_;
+    });
 
-    return if_
+    return show
         ? ((memo ? useMemo(() => children, []) : children) as () => ReactElement)()
         : else_
-        ? (else_ as () => ReactElement)()
+        ? isFunction(else_)
+            ? (else_ as () => ReactElement)()
+            : else_
         : null;
-});
+}
 
-export const For = observer(function For<T extends { id: string } | { _id: string } | { __id: string }>({
+export function For<
+    T extends ObservableObject<{ id: string | number } | { _id: string | number } | { __id: string | number }>
+>({
     each,
     optimized,
     item,
     children,
 }: {
-    each?: ObservableReadable<T[]>;
+    each?: T[];
     optimized?: boolean;
-    item?: ({ item: T }) => ReactElement;
+    item?: (props: { item: T }) => ReactElement;
     children?: (value: T) => ReactElement;
 }): ReactElement {
+    if (!each) return null;
+
+    // Get the raw value with a shallow listener so this list only re-renders
+    // when the array length changes
+    const v = useComputed(
+        () =>
+            (each as ObservableObject).get(optimized ? Tracking.optimized : Tracking.shallow) as {
+                id?: string;
+                _id?: string;
+                __id?: string;
+            }[]
+    );
+
+    if (!v) return null;
+
     // The child function gets wrapped in a memoized observer component
     if (!item && children) {
         // Update the ref so the generated component uses the latest function
@@ -59,18 +81,6 @@ export const For = observer(function For<T extends { id: string } | { _id: strin
 
         item = useMemo(() => observer(({ item }) => refChildren.current(item)), []);
     }
-
-    if (!each) return null;
-
-    // Get the raw value with a shallow listener so this list only re-renders
-    // when the array length changes
-    const v = each.get(optimized ? Tracking.optimized : Tracking.shallow) as {
-        id?: string;
-        _id?: string;
-        __id?: string;
-    }[];
-
-    if (!v) return null;
 
     // Get the appropriate id field
     const id = v.length > 0 ? (v[0].id ? 'id' : v[0]._id ? '_id' : v[0].__id ? '__id' : undefined) : undefined;
@@ -86,4 +96,4 @@ export const For = observer(function For<T extends { id: string } | { _id: strin
     }
 
     return out as unknown as ReactElement;
-});
+}
