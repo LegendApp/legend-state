148bb3952e1123b2bf32837225568a6eb4cb2330
Jay Meistrich <jmeistrich@gmail.com>
2022-09-13 19:27:23 +0800

    Fix react bundle size checker

--- testbundles/bundlereact.js
+++ testbundles/bundlereact.js
@@ -1,5 +1,5 @@
 import { observable } from '../dist/index.mjs';
-import { observer } from '../dist/react.mjs';
+import { enableLegendStateReact } from '../dist/react.mjs';
 
 observable(0);
-observer();
+console.log(enableLegendStateReact);
