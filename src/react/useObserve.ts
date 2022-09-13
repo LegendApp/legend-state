b04be728bcddeead7abe2a5fbed483f1c176adc1
Jay Meistrich <jmeistrich@gmail.com>
2022-09-13 19:27:12 +0800

    Fix useObserve should just be a wrapper around observe()

--- src/react/useObserve.ts
+++ src/react/useObserve.ts
@@ -1,5 +1,29 @@
-import { useComputed } from './useComputed';
+import { observe, setupTracking, tracking } from '@legendapp/state';
+import { useEffect } from 'react';
 
-export function useObserve<T>(selector: () => T) {
-    return useComputed(selector, false);
+export function useObserve(selector: () => void) {
+    // Development-only workaround for React 18 double calling useEffect
+    if (process.env.NODE_ENV === 'development') {
+        let cachedNodes;
+        const update = () => {
+            selector();
+            cachedNodes = tracking.nodes;
+        };
+        let dispose = observe(update);
+        useEffect(() => {
+            // Workaround for React 18's double calling useEffect. If this is the
+            // second useEffect, set up tracking again.
+            if (dispose === undefined) {
+                dispose = setupTracking(cachedNodes, update, /*noArgs*/ true);
+            }
+            return () => {
+                dispose();
+                dispose = undefined;
+            };
+        });
+    } else {
+        const dispose = observe(selector);
+        // Return dispose to cleanup before each render or on unmount
+        useEffect(() => dispose);
+    }
 }
