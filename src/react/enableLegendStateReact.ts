3f42ad8284dc6682bf8f0b21422630dcaae89d53
Jay Meistrich <jmeistrich@gmail.com>
2022-09-14 19:03:43 +0800

    Minor optimizations

--- src/react/enableLegendStateReact.ts
+++ src/react/enableLegendStateReact.ts
@@ -16,6 +16,8 @@ import {
 } from 'react';
 let isEnabled = false;
 
+const Updater = (s) => s + 1;
+
 export function enableLegendStateReact() {
     if (!isEnabled) {
         isEnabled = true;
@@ -53,75 +55,77 @@ export function enableLegendStateReact() {
                 return dispatcher;
             },
             set(newDispatcher) {
-                // When the React render is complete it sets the dispatcher to an object where useCallback has a length of 0
-                if (dispatcher && didBeginTracking && newDispatcher?.useCallback.length < 2) {
-                    didBeginTracking = false;
-                    // If the previous dispatcher tracked nodes then set up hooks
-                    if (tracking.nodes) {
-                        try {
-                            let [, forceRender] = dispatcher.useReducer((s) => s + 1, 0);
+                if (newDispatcher) {
+                    const useCallback = newDispatcher.useCallback;
+                    // When the React render is complete it sets the dispatcher to an object where useCallback has a length of 0
+                    if (dispatcher && didBeginTracking && useCallback.length < 2) {
+                        didBeginTracking = false;
+                        // If the previous dispatcher tracked nodes then set up hooks
+                        if (tracking.nodes) {
+                            try {
+                                let forceRender = dispatcher.useReducer(Updater, 0)[1];
 
-                            let noArgs = true;
-                            if (process.env.NODE_ENV === 'development') {
-                                tracking.listeners?.(tracking.nodes);
-                                if (tracking.updates) {
-                                    noArgs = false;
-                                    forceRender = tracking.updates(forceRender);
+                                let noArgs = true;
+                                if (process.env.NODE_ENV === 'development') {
+                                    tracking.listeners?.(tracking.nodes);
+                                    if (tracking.updates) {
+                                        noArgs = false;
+                                        forceRender = tracking.updates(forceRender);
+                                    }
+                                }
+
+                                // Track all of the nodes accessed during the dispatcher
+                                let dispose = setupTracking(tracking.nodes, forceRender, /*noArgs*/ noArgs);
+
+                                if (process.env.NODE_ENV === 'development') {
+                                    // Clear tracing
+                                    tracking.listeners = undefined;
+                                    tracking.updates = undefined;
+
+                                    const cachedNodes = tracking.nodes;
+                                    dispatcher.useEffect(() => {
+                                        // Workaround for React 18's double calling useEffect. If this is the
+                                        // second useEffect, set up tracking again.
+                                        if (dispose === undefined) {
+                                            dispose = setupTracking(cachedNodes, forceRender, /*noArgs*/ noArgs);
+                                        }
+                                        return () => {
+                                            dispose();
+                                            dispose = undefined;
+                                        };
+                                    });
+                                } else {
+                                    // Return dispose to cleanup before each render or on unmount
+                                    dispatcher.useEffect(() => dispose);
+                                }
+                            } catch (err) {
+                                // This may not ever be an error but since this is new we'll leave this here
+                                // for a bit while we see what the behavior is like
+                                if (process.env.NODE_ENV === 'development') {
+                                    console.error('[legend-state] error creating hooks', err);
+                                    throw new Error('[legend-state] error creating hooks');
                                 }
                             }
-
-                            // Track all of the nodes accessed during the dispatcher
-                            let dispose = setupTracking(tracking.nodes, forceRender, /*noArgs*/ noArgs);
-
-                            if (process.env.NODE_ENV === 'development') {
-                                // Clear tracing
-                                tracking.listeners = undefined;
-                                tracking.updates = undefined;
-
-                                const cachedNodes = tracking.nodes;
-                                dispatcher.useEffect(() => {
-                                    // Workaround for React 18's double calling useEffect. If this is the
-                                    // second useEffect, set up tracking again.
-                                    if (dispose === undefined) {
-                                        dispose = setupTracking(cachedNodes, forceRender, /*noArgs*/ noArgs);
-                                    }
-                                    return () => {
-                                        dispose();
-                                        dispose = undefined;
-                                    };
-                                });
-                            } else {
-                                // Return dispose to cleanup before each render or on unmount
-                                dispatcher.useEffect(() => dispose);
-                            }
-                        } catch (err) {
-                            // This may not ever be an error but since this is new we'll leave this here
-                            // for a bit while we see what the behavior is like
-                            if (process.env.NODE_ENV === 'development') {
-                                console.error('[legend-state] error creating hooks', err);
-                                throw new Error('[legend-state] error creating hooks');
-                            }
                         }
+                        // Restore the previous tracking context
+                        endTracking(prevNodes);
                     }
-                    // Restore the previous tracking context
-                    endTracking(prevNodes);
-                }
-                dispatcher = newDispatcher;
+                    dispatcher = newDispatcher;
 
-                // Start a new tracking context when entering a new rendering dispatcher
-                // In development, rendering dispatchers have useCallback named either "mountHookTypes" or "updateHookTypes"
-                // In production, they just have length = 2
-                if (
-                    newDispatcher &&
-                    !tracking.isTracking &&
-                    (process.env.NODE_ENV === 'development'
-                        ? !newDispatcher.useCallback.toString().includes('Invalid')
-                        : newDispatcher.useCallback.length === 2)
-                ) {
-                    didBeginTracking = true;
+                    // Start a new tracking context when entering a new rendering dispatcher
+                    // In development, rendering dispatchers have useCallback named either "mountHookTypes" or "updateHookTypes"
+                    // In production, they just have length = 2
+                    if (
+                        !tracking.isTracking &&
+                        (process.env.NODE_ENV === 'development'
+                            ? !useCallback.toString().includes('Invalid')
+                            : useCallback.length === 2)
+                    ) {
+                        didBeginTracking = true;
 
-                    // Keep a copy of the previous tracking context
-                    prevNodes = beginTracking();
+                        // Keep a copy of the previous tracking context
+                        prevNodes = beginTracking();
+                    }
                 }
             },
         });
