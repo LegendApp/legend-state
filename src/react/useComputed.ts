a76db7b59f9e6d6e7d06731ce37bf36fce50478f
Jay Meistrich <jmeistrich@gmail.com>
2022-10-24 16:45:56 +0800

    Add a deps array to useComputed so it can be updated if dependencies change

--- src/react/useComputed.ts
+++ src/react/useComputed.ts
@@ -1,15 +1,9 @@
 import { computed, ObservableComputed } from '@legendapp/state';
-import { useRef } from 'react';
+import { useMemo, useRef } from 'react';
 
-export function useComputed<T>(compute: () => T | Promise<T>): ObservableComputed<T> {
-    const ref = useRef<{ computed?: ObservableComputed<T>; compute?: () => T | Promise<T> }>({});
+export function useComputed<T>(compute: () => T | Promise<T>, deps?: any[]): ObservableComputed<T> {
+    const ref = useRef<{ compute?: () => T | Promise<T> }>({});
     ref.current.compute = compute;
 
-    let comp = ref.current.computed;
-
-    if (!comp) {
-        comp = ref.current.computed = computed(() => ref.current.compute());
-    }
-
-    return comp;
+    return useMemo(() => computed(() => ref.current.compute()), deps || []);
 }
