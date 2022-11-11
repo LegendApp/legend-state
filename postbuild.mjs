2d33bf112c3287e4e93cd42da30904fe9e1afa72
Jay Meistrich <jmeistrich@gmail.com>
2022-11-11 17:34:57 +0800

    Improve the postbuild script to rewrite the wildcard exports in dist/package.json to include all of the individual exports, because some systems don't support wildcard exports

--- postbuild.mjs
+++ postbuild.mjs
@@ -11,15 +11,41 @@ Object.keys(pkg.exports).forEach((exp) => {
         const files = fs.readdirSync(path.join('src', expPath));
         const distPath = path.join('dist', expPath);
         const distTempPath = path.join(distPath, 'temp');
+
+        const exportObj = pkg.exports[exp];
         files.forEach((file) => {
             const base = path.parse(file).name;
+
+            // Add an export for each of the files in the folder
+            const exportPath = expPath + '/' + base;
+            const newExportObj = (pkg.exports[exportPath] = Object.assign({}, exportObj));
+            Object.keys(newExportObj).forEach((key) => {
+                newExportObj[key] = newExportObj[key].replace('*', base);
+            });
+
+            // Copy files in the wildcard to the output directory
             extensions.map((ext) => {
                 const filename = base + '.' + ext;
+
                 fs.cpSync(path.join(distTempPath, filename), path.join(distPath, filename));
             });
+            // Copy type file in the wildcard to the output directory
+
             const dts = base + '.d.ts';
             fs.cpSync(path.join(distTempPath, 'src', expPath, dts), path.join(distPath, dts));
         });
+
+        // Delete the wildcard export
+        delete pkg.exports[exp];
+
+        // Delete the temp path
         fs.rmSync(distTempPath, { recursive: true, force: true });
     }
 });
+
+// Remove fields we don't want in the built version
+pkg.private = false;
+delete pkg.devDependencies;
+delete pkg.scripts;
+
+fs.writeFileSync(path.join('dist/package.json'), JSON.stringify(pkg, undefined, 4));
