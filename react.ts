486c8b7a4ee655401b058057e5eaac98b7b1c248
Jay Meistrich <jmeistrich@gmail.com>
2022-11-11 22:40:48 +0800

    Move createObservableHook to react-hooks and add useReducer overriding

--- react.ts
+++ react.ts
@@ -1,13 +1,12 @@
-export * from './src/react/createObservableHook';
 export * from './src/react/enableLegendStateReact';
 export * from './src/react/flow';
+export * from './src/react/lifecycle';
 export * from './src/react/reactive-observer';
 export * from './src/react/useComputed';
 export * from './src/react/useObservable';
 export * from './src/react/useObservableReducer';
 export * from './src/react/useObserve';
 export * from './src/react/useSelector';
-export * from './src/react/lifecycle';
 
 import type { ReactFragment } from 'react';
 
---
+++ src/react-hooks/createObservableHook.ts
@@ -0,0 +1,53 @@
+import { isFunction, Observable, observable } from '@legendapp/state';
+import React, { MutableRefObject, ReducerState } from 'react';
+
+function overrideHooks<TRet>(refObs: MutableRefObject<Observable<TRet>>) {
+    // @ts-ignore
+    React.useState = function useState(initialState: TRet | (() => TRet)) {
+        const obs =
+            refObs.current ??
+            (refObs.current = observable(isFunction(initialState) ? initialState() : initialState) as Observable<TRet>);
+        return [obs.get() as TRet, obs.set] as [TRet, React.Dispatch<React.SetStateAction<TRet>>];
+    };
+    // @ts-ignore
+    React.useReducer = function useReducer<R extends Reducer<any, any>>(
+        reducer: R,
+        initializerArg: ReducerState<R>,
+        initializer: (arg: ReducerState<R>) => ReducerState<R>
+    ) {
+        const obs =
+            refObs.current ??
+            (refObs.current = observable(
+                initializerArg !== undefined && isFunction(initializerArg)
+                    ? initializer(initializerArg)
+                    : initializerArg
+            ) as Observable<TRet>);
+        const dispatch = (action) => {
+            obs.set(reducer(obs.get(), action));
+        };
+        return [obs, dispatch];
+    };
+}
+
+export function createObservableHook<TArgs extends any[], TRet>(
+    fn: (...args: TArgs) => TRet
+): (...args: TArgs) => Observable<TRet> {
+    const _useState = React.useState;
+    const _useReducer = React.useReducer;
+
+    return function (...args: TArgs) {
+        const refObs = React.useRef<Observable<TRet>>();
+
+        // First override the built-in hooks to create/update observables
+        overrideHooks(refObs);
+
+        // Then call the original hook
+        fn(...args);
+
+        // And reset back to the built-in hooks
+        React.useState = _useState;
+        React.useReducer = _useReducer;
+
+        return refObs.current;
+    };
+}
