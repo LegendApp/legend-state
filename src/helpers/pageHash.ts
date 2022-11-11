359ca83aaf5e08fe1256d7bd9cf579b1eeb1401b
Jay Meistrich <jmeistrich@gmail.com>
2022-11-11 19:35:58 +0800

    Add pageHash and pageHashParams observable helpers, remove useHash

---
+++ src/helpers/pageHash.ts
@@ -0,0 +1,41 @@
+import { observable, Observable } from '@legendapp/state';
+
+interface Options {
+    setter: 'pushState' | 'replaceState' | 'hash';
+}
+let _options: Options = { setter: 'hash' };
+
+function configurePageHash(options: Options) {
+    _options = options;
+}
+
+const hasWindow = typeof window !== 'undefined';
+const pageHash: Observable<string> = observable(hasWindow ? window.location.hash.slice(1) : '');
+
+if (hasWindow) {
+    let isSetting = false;
+    // Set the page hash when the observable changes
+    pageHash.onChange((value) => {
+        if (!isSetting) {
+            const hash = '#' + value;
+            const setter = _options?.setter || 'hash';
+            if (setter === 'pushState') {
+                history.pushState(null, null, hash);
+            } else if (setter === 'replaceState') {
+                history.replaceState(null, null, hash);
+            } else {
+                location.hash = hash;
+            }
+        }
+    });
+    // Update the observable whenever the hash changes
+    const cb = () => {
+        isSetting = true;
+        pageHash.set(window.location.hash.slice(1));
+        isSetting = false;
+    };
+    // Subscribe to window hashChange event
+    window.addEventListener('hashchange', cb);
+}
+
+export { pageHash, configurePageHash };
---
+++ src/helpers/pageHashParams.ts
@@ -0,0 +1,55 @@
+import { observable, Observable } from '@legendapp/state';
+
+interface Options {
+    setter: 'pushState' | 'replaceState' | 'hash';
+}
+let _options: Options = { setter: 'hash' };
+
+function configurePageHashParams(options: Options) {
+    _options = options;
+}
+
+function toParams(str: string) {
+    const ret: Record<string, string> = {};
+    const searchParams = new URLSearchParams(str);
+    for (const [key, value] of searchParams) {
+        ret[key] = value;
+    }
+    return ret;
+}
+function toString(params: Record<string, string>) {
+    return new URLSearchParams(params).toString();
+}
+
+const hasWindow = typeof window !== 'undefined';
+const pageHashParams: Observable<Record<string, string>> = observable(
+    hasWindow ? toParams(window.location.hash.slice(1)) : {}
+);
+
+if (hasWindow) {
+    let isSetting = false;
+    // Set the page hash when the observable changes
+    pageHashParams.onChange((value) => {
+        if (!isSetting) {
+            const hash = '#' + toString(value);
+            const setter = _options?.setter || 'hash';
+            if (setter === 'pushState') {
+                history.pushState(null, null, hash);
+            } else if (setter === 'replaceState') {
+                history.replaceState(null, null, hash);
+            } else {
+                location.hash = hash;
+            }
+        }
+    });
+    // Update the observable whenever the hash changes
+    const cb = () => {
+        isSetting = true;
+        pageHashParams.set(toParams(window.location.hash.slice(1)));
+        isSetting = false;
+    };
+    // Subscribe to window hashChange event
+    window.addEventListener('hashchange', cb);
+}
+
+export { pageHashParams, configurePageHashParams };
