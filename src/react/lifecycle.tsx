fe420c05671e883df432ca31c17ca1b36303829b
Jay Meistrich <jmeistrich@gmail.com>
2022-11-12 13:24:22 +0800

    Improve type of useMount to take a Destructor too

--- src/react/lifecycle.tsx
+++ src/react/lifecycle.tsx
@@ -1,6 +1,7 @@
+import type { EffectCallback } from 'react';
 import { useEffect } from 'react';
 
-export function useMount(fn: () => void) {
+export function useMount(fn: EffectCallback) {
     return useEffect(fn, []);
 }
 
