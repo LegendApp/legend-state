962f547f4c31dcd6206b9431c24f8af4e00ef96a
Jay Meistrich <jmeistrich@gmail.com>
2022-11-20 02:03:25 +0800

    Change when to wait on empty objects

--- src/when.ts
+++ src/when.ts
@@ -1,4 +1,5 @@
 import { computeSelector } from './helpers';
+import { isEmpty, isObject } from './is';
 import type { ObserveEvent, Selector } from './observableInterfaces';
 import { observe } from './observe';
 
@@ -9,7 +10,7 @@ export function when<T>(predicate: Selector<T>, effect?: (value: T) => any | (()
     function run(e: ObserveEvent<T>) {
         const ret = computeSelector(predicate);
 
-        if (ret) {
+        if (ret && !(isObject(ret) && isEmpty(ret))) {
             // If value is truthy then run the effect
             effect!(ret);
 
