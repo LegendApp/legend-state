c6be3c37bd884c69f1c5e4e2737e18f6393f7be8
Jay Meistrich <jmeistrich@gmail.com>
2022-11-28 23:53:48 +0800

    Change when to return the value directly if it's already resolved on the first run

--- src/when.ts
+++ src/when.ts
@@ -6,28 +6,35 @@ import { observe } from './observe';
 export function when<T>(predicate: Selector<T>): Promise<T>;
 export function when<T>(predicate: Selector<T>, effect: (value: T) => any | (() => any)): () => void;
 export function when<T>(predicate: Selector<T>, effect?: (value: T) => any | (() => any)) {
+    let value: T;
     // Create a wrapping fn that calls the effect if predicate returns true
     function run(e: ObserveEvent<T>) {
         const ret = computeSelector(predicate);
 
         if (ret && !((isObject(ret) && isEmpty(ret)) || (isArray(ret) && ret.length === 0))) {
+            value = ret;
             // If value is truthy then run the effect
-            effect!(ret);
+            effect?.(ret);
 
             // Set cancel so that observe does not track
             e.cancel = true;
         }
     }
-
-    // If no effect parameter return a promise
-    const promise =
-        !effect &&
-        new Promise<T>((resolve) => {
-            effect = resolve;
-        });
-
     // Create an effect for the fn
     const dispose = observe(run);
 
-    return promise || dispose;
+    // If first run resulted in a truthy value just return it
+    // It will have set e.cancel so no need to dispose
+    if (value !== undefined) {
+        return value;
+    } else {
+        // If no effect parameter return a promise
+        const promise =
+            !effect &&
+            new Promise<T>((resolve) => {
+                effect = resolve;
+            });
+
+        return promise || dispose;
+    }
 }
