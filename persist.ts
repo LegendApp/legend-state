285c47220033d9ef0c4c02b7efb6009cab95bd65
Jay Meistrich <jmeistrich@gmail.com>
2022-12-09 14:53:10 +0800

    Rename invertMap to invertFieldMap to be more descriptive

--- persist.ts
+++ persist.ts
@@ -2,7 +2,7 @@ export {
     configureObservablePersistence,
     observablePersistConfiguration,
 } from './src/persist/configureObservablePersistence';
-export { invertMap, transformObject, transformPath } from './src/persist/fieldTransformer';
+export { invertFieldMap, transformObject, transformPath } from './src/persist/fieldTransformer';
 /** @internal */
 export { getDateModifiedKey } from './src/persist/persistHelpers';
 export { mapPersistences, persistObservable, persistState } from './src/persist/persistObservable';
