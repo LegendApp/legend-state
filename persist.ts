7ab3ea854d39473187a8adfe07e0482189cbf1cb
Jay Meistrich <jmeistrich@gmail.com>
2023-01-09 13:01:46 +1300

    Export onChangeRemote function for external persistence libraries to use

--- persist.ts
+++ persist.ts
@@ -4,7 +4,7 @@ export {
 } from './src/persist/configureObservablePersistence';
 export { invertFieldMap, transformObject, transformPath } from './src/persist/fieldTransformer';
 export { getDateModifiedKey } from './src/persist/persistHelpers';
-export { mapPersistences, persistObservable, persistState } from './src/persist/persistObservable';
+export { mapPersistences, onChangeRemote, persistObservable, persistState } from './src/persist/persistObservable';
 
 import { tracking } from './src/tracking';
 
