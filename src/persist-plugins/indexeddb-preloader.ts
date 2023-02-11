9f02e48a1353bad6e228842b082aa64626aa0cc3
Jay Meistrich <jmeistrich@gmail.com>
2023-02-11 00:55:52 -0800

    Fixed isArray and isObject not working in indexeddb-preloader because it was outside of the worker code

--- src/persist-plugins/indexeddb-preloader.ts
+++ src/persist-plugins/indexeddb-preloader.ts
@@ -1,12 +1,5 @@
 import type { FieldTransforms, PersistMetadata } from '../observableInterfaces';
 
-function isArray(obj: unknown): obj is Array<any> {
-    return Array.isArray(obj);
-}
-function isObject(obj: unknown): obj is Record<any, any> {
-    return !!obj && typeof obj === 'object' && !isArray(obj);
-}
-
 export function preloadIndexedDB({
     databaseName,
     tableNames,
@@ -59,6 +52,13 @@ export function preloadIndexedDB({
         const tableMetadata: Record<string, any> = {};
         let db: IDBDatabase;
 
+        function isArray(obj: unknown): obj is Array<any> {
+            return Array.isArray(obj);
+        }
+        function isObject(obj: unknown): obj is Record<any, any> {
+            return !!obj && typeof obj === 'object' && !isArray(obj);
+        }
+
         self.onmessage = function onmessage(
             e: MessageEvent<[string, string[], string[], number, Record<string, FieldTransforms<any>>]>
         ) {
@@ -83,19 +83,17 @@ export function preloadIndexedDB({
                             } else {
                                 const mapped = map[key];
                                 if (mapped === undefined) {
-                                    ret[key] = v;
-                                    if (
-                                        (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') &&
-                                        map[key] === undefined &&
-                                        // Run without error if it's the Firebase dateModified key
-                                        key !== '@'
-                                    ) {
-                                        console.error(
-                                            'A fatal field transformation error has occurred',
-                                            key,
-                                            dataIn,
-                                            map
-                                        );
+                                    // Don't transform dateModified if user doesn't want it
+                                    if (key !== '@') {
+                                        ret[key] = v;
+                                        if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
+                                            console.error(
+                                                'A fatal field transformation error has occurred',
+                                                key,
+                                                dataIn,
+                                                map
+                                            );
+                                        }
                                     }
                                 } else if (mapped !== null) {
                                     if (v !== undefined && v !== null) {
