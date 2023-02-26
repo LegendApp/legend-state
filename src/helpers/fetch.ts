c54f9edf6593ee3f6094bdd6e4b39a6dfcf32609
Matthew Turner <matthew.m.turner@outlook.com>
2023-02-26 00:53:43 -0500

    Start working on pagination

--- src/helpers/fetch.ts
+++ src/helpers/fetch.ts
@@ -1,9 +1,33 @@
-import { observable } from '@legendapp/state';
+import { observable, ObservableObject } from '@legendapp/state';
 
-export function observableFetch<T extends unknown>(
+// export function observableFetch<T extends unknown>(
+//     input: RequestInfo | URL,
+//     init?: RequestInit,
+//     valueType?: 'arrayBuffer' | 'blob' | 'formData' | 'json' | 'text'
+// ): ObservableObject<{
+//     data?: T;
+//     error?: any;
+//     errorStr?: string;
+//     loading: boolean;
+// }>;
+
+// export function observableFetch<T extends any[]>(
+//     input: RequestInfo | URL,
+//     init?: RequestInit,
+//     valueType?: 'arrayBuffer' | 'blob' | 'formData' | 'json' | 'text',
+//     paginateFn?: Function // Function that returns the total number of pages
+// ): ObservableObject<{
+//     data?: T;
+//     error?: any;
+//     errorStr?: string;
+//     loading: boolean;
+// }>;
+
+export function observableFetch<T extends unknown | any[]>(
     input: RequestInfo | URL,
     init?: RequestInit,
-    valueType?: 'arrayBuffer' | 'blob' | 'formData' | 'json' | 'text'
+    valueType?: 'arrayBuffer' | 'blob' | 'formData' | 'json' | 'text',
+    paginateFn?: Function // Function that returns the total number of pages
 ) {
     const obs = observable<{
         data?: T;
@@ -17,10 +41,35 @@ export function observableFetch<T extends unknown>(
         loading: true,
     });
 
-    fetch(input, init)
-        .then((response) => response[valueType || 'json']())
-        .then((value) => obs.set({ data: value as T, loading: false }))
-        .catch((error) => obs.set({ loading: false, error, errorStr: error?.toString?.() }));
+    if (!paginateFn) {
+        fetch(input, init)
+            .then((response) => response[valueType || 'json']())
+            .then((value) => obs.set({ data: value as T, loading: false }))
+            .catch((error) => obs.set({ loading: false, error, errorStr: error?.toString?.() }));
+    } else {
+        function createUrls(pages: number) {
+            const urls: string[] = [];
+            for (let i = 2; i <= pages; i++) {
+                urls.push(`${input}?page=${i}`);
+            }
+            return urls;
+        }
+
+        fetch(input, init)
+            .then((response) => response[valueType || 'json']())
+            .then((value) => {
+                const pages = paginateFn(value);
+                if (pages > 1) {
+                    const urls = createUrls(pages);
+                    Promise.all(urls.map((url) => fetch(url, init).then((response) => response['json']())))
+                        .then((values) => {
+                            const data = [value, ...values].flat();
+                            obs.set({ data, loading: false });
+                        })
+                        .catch((error) => obs.set({ loading: false, error, errorStr: error?.toString?.() }));
+                }
+            });
+    }
 
     return obs;
 }
--- tests/helpers.test.ts
+++ tests/helpers.test.ts
@@ -1,6 +1,8 @@
 import { symbolDelete } from '../internal';
 import { isObservable, mergeIntoObservable } from '../src/helpers';
 import { observable } from '../src/observable';
+import { observableFetch } from '../src/helpers/fetch';
+import { observe } from '../src/observe';
 
 describe('mergeIntoObservable', () => {
     it('should merge two plain objects', () => {
@@ -33,5 +35,69 @@ describe('mergeIntoObservable', () => {
         const merged = mergeIntoObservable(target, source);
         expect(merged).toEqual({ a: 1 });
         expect(isObservable(merged)).toBe(false);
+    });
+});
+
+type TestUser = {
+    id: number;
+    email: string;
+    first_name: string;
+    last_name: string;
+    avatar: string;
+};
+
+type TestApiResponse = {
+    data: TestUser | TestUser[];
+    support?: {
+        text: string;
+        url: string;
+    };
+    page?: number;
+    per_page?: number;
+    total?: number;
+    total_pages?: number;
+};
+
+describe('observableFetch', () => {
+    it('should return correct status and data for a user', async () => {
+        const response = observableFetch<TestApiResponse>('https://reqres.in/api/users/1');
+        setTimeout(() => {
+            observe(() => {
+                const { loading, data, error, errorStr } = response.get();
+                if (loading) console.log('Loading...');
+                else if (error) console.log('Error: ', errorStr);
+                else {
+                    expect(data).toEqual({
+                        data: {
+                            id: 1,
+                            email: 'george.bluth@reqres.in',
+                            first_name: 'George',
+                            last_name: 'Bluth',
+                            avatar: 'https://reqres.in/img/faces/1-image.jpg',
+                        },
+                        support: {
+                            text: 'To keep ReqRes free, contributions towards server costs are appreciated!',
+                            url: 'https://reqres.in/#support-heading',
+                        },
+                    });
+                }
+            });
+        }, 200);
+    });
+
+    it('should return all data from paginated api', async () => {
+        const response = observableFetch<TestApiResponse>('https://reqres.in/api/users');
+        setTimeout(() => {
+            observe(() => {
+                const { loading, data, error, errorStr } = response.get();
+                if (loading) console.log('Loading...');
+                else if (error) console.log('Error: ', errorStr);
+                else {
+                    const { total } = data;
+                    const { data: users } = data;
+                    expect(users).toHaveLength(total);
+                }
+            });
+        }, 1000);
     });
 });
