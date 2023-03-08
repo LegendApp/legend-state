5ef6e16796e6f7d50ec426b1c1ccc405d70b99da
Jay Meistrich <jmeistrich@gmail.com>
2023-03-08 22:38:00 +0900

    Change Show to check "if" with normal behavior by default, but check whether it has data in it with ifHasData prop

--- src/react/Show.tsx
+++ src/react/Show.tsx
@@ -1,30 +1,39 @@
 import type { Selector } from '@legendapp/state';
-import { isObservableValueReady } from '@legendapp/state';
+import { computeSelector, isObservableValueReady } from '@legendapp/state';
 import { createElement, FC, ReactElement, ReactNode } from 'react';
 import { useSelector } from './useSelector';
 
-export function Show<T>(props: {
+interface PropsIf<T> {
     if: Selector<T>;
+    ifHasData?: never;
+}
+interface PropsIfHasData<T> {
+    if?: never;
+    ifHasData: Selector<T>;
+}
+
+interface PropsBase {
     else?: ReactNode | (() => ReactNode);
     wrap?: FC;
     children: ReactNode | ((value?: T) => ReactNode);
-}): ReactElement;
-export function Show<T>({
-    if: if_,
-    else: else_,
-    wrap,
-    children,
-}: {
-    if: Selector<T>;
-    else?: ReactNode | (() => ReactNode);
-    wrap?: FC;
-    children: ReactNode | (() => ReactNode);
-}): ReactElement {
-    const value = useSelector<T>(if_);
+}
 
-    const child = useSelector(isObservableValueReady(value) ? children : else_ ? else_ : null, {
-        shouldRender: true,
-    }) as ReactElement;
+type Props<T> = PropsBase & (PropsIf<T> | PropsIfHasData<T>);
+
+export function Show<T>(props: Props<T>): ReactElement;
+export function Show<T>({ if: if_, ifHasData, else: else_, wrap, children }: Props<T>): ReactElement {
+    const child = useSelector(
+        ifHasData !== undefined
+            ? isObservableValueReady(computeSelector(ifHasData))
+            : computeSelector(if_)
+            ? children
+            : else_
+            ? else_
+            : null,
+        {
+            shouldRender: true,
+        }
+    ) as ReactElement;
 
     return wrap ? createElement(wrap, undefined, child) : child;
 }
