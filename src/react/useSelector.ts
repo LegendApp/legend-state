0d432a34856134f5f96106606d8063efb38435e0
Jay Meistrich <jmeistrich@gmail.com>
2023-03-20 00:03:37 +0900

    Optimize useSelector to memoize it all in a ref

--- src/react/useSelector.ts
+++ src/react/useSelector.ts
@@ -2,68 +2,72 @@ import { beginTracking, computeSelector, endTracking, Selector, setupTracking, t
 import { useRef } from 'react';
 import { useSyncExternalStore } from 'use-sync-external-store/shim';
 
-export function useSelector<T>(selector: Selector<T>): T {
-    const ref = useRef<{ version: number; value?: T; notify?: () => void; update?: () => void; dispose?: () => void }>({
-        version: 0,
-    });
+interface SelectorFunctions<T> {
+    subscribe: (onStoreChange: () => void) => void;
+    getVersion: () => number;
+    run: (selector: Selector<T>) => T;
+}
 
-    if (!selector) return selector as T;
+function createSelectorFunctions<T>(): SelectorFunctions<T> {
+    let version = 0;
+    let notify: () => void;
+    let dispose: () => void;
 
-    const current = ref.current;
+    const _update = () => {
+        version++;
+        notify?.();
+    };
 
-    // Compute the selector inside a tracking context
-    beginTracking();
-    current.value = computeSelector(selector);
-    const tracker = tracking.current;
-    const { nodes } = tracker;
-    endTracking();
+    return {
+        subscribe: (onStoreChange: () => void) => {
+            notify = onStoreChange;
 
-    let noArgs = true;
-    if (!current.update) {
-        current.update = () => {
-            current.version++;
-            current.notify?.();
-        };
-        // Do tracing if it was requested
-        if (process.env.NODE_ENV === 'development' && tracker && nodes) {
-            tracker.traceListeners?.(nodes);
-            if (tracker.traceUpdates) {
-                noArgs = false;
-                current.update = tracker.traceUpdates(current.update) as () => void;
-            }
-            // Clear tracing so it doesn't leak to other components
-            tracker.traceListeners = undefined;
-            tracker.traceUpdates = undefined;
-        }
-    }
-
-    // Dispose if already listening
-    current.dispose?.();
-    // useSyncExternalStore doesn't subscribe until after the component mount.
-    // We want to subscribe immediately so we don't miss any updates
-    current.dispose = setupTracking(nodes, current.update, noArgs);
-
-    // Returning a version number lets us have mutable values. When we know an observable
-    // is updated we increment the value so that the useSyncExternalStore updates with the new value.
-    const getVersion = () => current.version;
-
-    useSyncExternalStore(
-        (onStoreChange: () => void) => {
-            const current = ref.current;
-            current.notify = onStoreChange;
-            // Listen if not already listening
-            if (!current.dispose) {
-                current.dispose = setupTracking(nodes, current.update, noArgs);
-            }
-
-            return () => {
-                current.dispose?.();
-                current.dispose = undefined;
-            };
+            return () => dispose?.();
         },
-        getVersion,
-        getVersion
-    );
+        getVersion: () => version,
+        run: (selector: Selector<T>) => {
+            // Compute the selector inside a tracking context
+            beginTracking();
+            const value = selector ? computeSelector(selector) : selector;
+            const tracker = tracking.current;
+            const { nodes } = tracker;
+            endTracking();
 
-    return current.value;
+            let noArgs = true;
+            let update = _update;
+            // Do tracing if it was requested
+            if (process.env.NODE_ENV === 'development' && tracker && nodes) {
+                tracker.traceListeners?.(nodes);
+                if (tracker.traceUpdates) {
+                    noArgs = false;
+                    update = tracker.traceUpdates(_update) as () => void;
+                }
+                // Clear tracing so it doesn't leak to other components
+                tracker.traceListeners = undefined;
+                tracker.traceUpdates = undefined;
+            }
+
+            // Dispose if already listening
+            dispose?.();
+            // useSyncExternalStore doesn't subscribe until after the component mount.
+            // We want to subscribe immediately so we don't miss any updates
+            dispose = setupTracking(nodes, update, noArgs);
+
+            return value;
+        },
+    };
+}
+
+export function useSelector<T>(selector: Selector<T>): T {
+    const ref = useRef<SelectorFunctions<T>>();
+    if (!ref.current) {
+        ref.current = createSelectorFunctions<T>();
+    }
+    const { subscribe, getVersion, run } = ref.current;
+
+    const value = run(selector);
+
+    useSyncExternalStore(subscribe, getVersion, getVersion);
+
+    return value;
 }
