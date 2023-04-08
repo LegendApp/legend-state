f673a0885494cb12739dc6f275aba27bd0e8f44e
Jay Meistrich <jmeistrich@gmail.com>
2023-04-08 17:14:10 +0900

    Remove the early out in fieldTransformer that isn't needed anymore

--- src/persist/fieldTransformer.ts
+++ src/persist/fieldTransformer.ts
@@ -41,8 +41,6 @@ export function transformObject(dataIn: Record<string, any>, map: Record<string,
         const dict = Object.keys(map).length === 1 && map['_dict'];
 
         for (const key in dataIn) {
-            if (ret[key] !== undefined) continue;
-
             let v = dataIn[key];
 
             if (dict) {
