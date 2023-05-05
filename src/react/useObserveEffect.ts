cf7f1062b34b3306ec0b8570a3af9944608df876
Jay Meistrich <jmeistrich@gmail.com>
2023-05-05 16:49:06 +0800

    fix: parameters and typing of useObserveEffect to match observe

--- src/react/useObserveEffect.ts
+++ src/react/useObserveEffect.ts
@@ -7,15 +7,27 @@ import {
     Selector,
 } from '@legendapp/state';
 import { useRef } from 'react';
+import type { ObserveOptions } from '../observe';
 import { useEffectOnce } from './useEffectOnce';
 
+export function useObserveEffect<T>(run: (e: ObserveEvent<T>) => T | void, options?: ObserveOptions): void;
 export function useObserveEffect<T>(
-    selector: ObservableReadable<T>,
-    callback: (e: ObserveEventCallback<T>) => any
+    selector: Selector<T>,
+    reaction?: (e: ObserveEventCallback<T>) => any,
+    options?: ObserveOptions
 ): void;
-export function useObserveEffect<T>(selector: (e: ObserveEvent<T>) => T | void): void;
-export function useObserveEffect<T>(selector: Selector<T>, reaction?: (e: ObserveEventCallback<T>) => any): void;
-export function useObserveEffect<T>(selector: Selector<T>, reaction?: (e: ObserveEventCallback<T>) => any): void {
+export function useObserveEffect<T>(
+    selector: Selector<T> | ((e: ObserveEvent<T>) => any),
+    reactionOrOptions?: ((e: ObserveEventCallback<T>) => any) | ObserveOptions,
+    options?: ObserveOptions
+): void {
+    let reaction: (e: ObserveEventCallback<T>) => any;
+    if (isFunction(reactionOrOptions)) {
+        reaction = reactionOrOptions;
+    } else {
+        options = reactionOrOptions;
+    }
+
     const ref = useRef<{
         selector: Selector<T> | ((e: ObserveEvent<T>) => T | void) | ObservableReadable<T>;
         reaction?: (e: ObserveEventCallback<T>) => any;
@@ -28,7 +40,8 @@ export function useObserveEffect<T>(selector: Selector<T>, reaction?: (e: Observ
                 const { selector } = ref.current as { selector: (e: ObserveEvent<T>) => T | void };
                 return isFunction(selector) ? selector(e) : selector;
             }) as any,
-            (e) => ref.current.reaction?.(e)
+            (e) => ref.current.reaction?.(e),
+            options
         )
     );
 }
