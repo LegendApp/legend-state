d07bc9857aa8b638745b95d460cc8551585e8985
Jay Meistrich <jmeistrich@gmail.com>
2023-05-14 23:57:40 +0800

    Fix typing of Switch was not working well for primitives

--- src/react/Switch.tsx
+++ src/react/Switch.tsx
@@ -2,13 +2,27 @@ import type { Selector } from '@legendapp/state';
 import { ReactElement, ReactNode } from 'react';
 import { useSelector } from './useSelector';
 
-export function Switch<T>({
+export function Switch<T extends object>({
     value,
     children,
 }: {
     value?: Selector<T>;
     children: Record<keyof T, () => ReactNode>;
-}): ReactNode {
+}): ReactElement;
+export function Switch<T extends string | number | symbol>({
+    value,
+    children,
+}: {
+    value?: Selector<T>;
+    children: Record<T, () => ReactNode>;
+}): ReactElement;
+export function Switch<T>({
+    value,
+    children,
+}: {
+    value?: Selector<T>;
+    children: Record<any, () => ReactNode>;
+}): ReactElement {
     // Select from an object of cases
     return ((children as Record<any, () => ReactNode>)[useSelector(value)]?.() ??
         (children as Record<any, () => ReactNode>)['default']?.() ??
