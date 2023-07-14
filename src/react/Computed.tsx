0bc64c23d65942e34ca4cffca25600370c488bc7
Jay Meistrich <jmeistrich@gmail.com>
2023-07-14 13:43:49 +0900

    Memo and Computed support observable children

--- src/react/Computed.tsx
+++ src/react/Computed.tsx
@@ -1,6 +1,7 @@
 import { ReactElement, ReactNode } from 'react';
+import type { Selector } from '../observableInterfaces';
 import { useSelector } from './useSelector';
 
-export function Computed({ children }: { children: () => ReactNode }): ReactElement {
+export function Computed({ children }: { children: Selector<ReactNode> }): ReactElement {
     return useSelector(children) as ReactElement;
 }
--- src/react/Memo.tsx
+++ src/react/Memo.tsx
@@ -1,8 +1,9 @@
 import { memo, ReactElement, ReactNode } from 'react';
+import type { Selector } from 'src/observableInterfaces';
 import { useSelector } from './useSelector';
 
 export const Memo = /*#__PURE__*/ memo(
-    function Memo({ children }: { children: () => ReactNode }): ReactElement {
+    function Memo({ children }: { children: Selector<ReactNode> }): ReactElement {
         return useSelector(children) as ReactElement;
     },
     () => true
