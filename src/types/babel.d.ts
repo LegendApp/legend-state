501cf1d152f91416f4fb6a432937968113d4a68e
Jay Meistrich <jmeistrich@gmail.com>
2023-07-22 20:16:42 +0900

    chore: fix formatting of babel.d.ts

--- src/types/babel.d.ts
+++ src/types/babel.d.ts
@@ -1,8 +1,12 @@
-import { ReactNode } from 'react';
 import type { ObservableReadable } from '@legendapp/state';
+import { ReactNode } from 'react';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import type { Computed, Memo } from '@legendapp/state/react';
 declare module '@legendapp/state/react' {
-    export declare const Computed: (props: { children: ObservableReadable | (() => ReactNode) | ReactNode }) => React.ReactElement;
-    export declare const Memo: (props: { children: ObservableReadable | (() => ReactNode) | ReactNode }) => React.ReactElement;
+    export declare const Computed: (props: {
+        children: ObservableReadable | (() => ReactNode) | ReactNode;
+    }) => React.ReactElement;
+    export declare const Memo: (props: {
+        children: ObservableReadable | (() => ReactNode) | ReactNode;
+    }) => React.ReactElement;
 }
