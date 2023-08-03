c8b0b8f0f3bee2c024e3ffa08eff3e1c71d7ad30
Jay Meistrich <jmeistrich@gmail.com>
2023-08-03 16:13:50 +0900

    add a todov2 comment

--- react-components.ts
+++ react-components.ts
@@ -1,2 +1,4 @@
 export { observer, reactive, reactiveObserver } from '@legendapp/state/react';
 export * from './src/react-components/react-components';
+
+// TODOV2 This shouldn't be exporting anything from /react
