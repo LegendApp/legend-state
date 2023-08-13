126cd624aedf06d597ce618a7fc555fa1a19350f
Jay Meistrich <jmeistrich@gmail.com>
2023-08-13 17:02:52 +0700

    remove: some duplicated exports from react-components

--- react-components.ts
+++ react-components.ts
@@ -1,4 +1,1 @@
-export { observer, reactive, reactiveObserver } from '@legendapp/state/react';
 export * from './src/react-components/react-components';
-
-// TODOV2 This shouldn't be exporting anything from /react
