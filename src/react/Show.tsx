54b9f99f0d0dec1f716a646d2ac5be96e7fbd033
Jay Meistrich <jmeistrich@gmail.com>
2023-08-17 13:07:05 +0700

    fix: Show was not passing value to children if it's a function

--- src/react/Show.tsx
+++ src/react/Show.tsx
@@ -1,6 +1,6 @@
 import type { Selector } from '@legendapp/state';
-import { computeSelector, isObservableValueReady } from '@legendapp/state';
-import { createElement, FC, ReactElement, ReactNode } from 'react';
+import { computeSelector, isFunction, isObservableValueReady } from '@legendapp/state';
+import { FC, ReactElement, ReactNode, createElement } from 'react';
 import { useSelector } from './useSelector';
 
 interface PropsIf<T> {
@@ -24,10 +24,15 @@ export function Show<T>(props: Props<T>): ReactElement;
 export function Show<T>({ if: if_, ifReady, else: else_, wrap, children }: Props<T>): ReactElement {
     const child = useSelector(() => {
         const value = computeSelector(if_ ?? ifReady);
-        const child = computeSelector(
-            (ifReady !== undefined ? isObservableValueReady(value) : value) ? children : else_ ? else_ : null,
+        return computeSelector(
+            (ifReady !== undefined ? isObservableValueReady(value) : value)
+                ? isFunction(children)
+                    ? () => children(value)
+                    : children
+                : else_
+                ? else_
+                : null,
         );
-        return child;
     });
     return wrap ? createElement(wrap, undefined, child) : child;
 }
