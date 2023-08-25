42d83034325cd9984843dec1381a1fbae0d2de90
Jay Meistrich <jmeistrich@gmail.com>
2023-08-25 22:16:48 +0700

    fix fetch to return the response value and use optional init params

--- src/persist-plugins/fetch.ts
+++ src/persist-plugins/fetch.ts
@@ -1,16 +1,30 @@
 import { ObservablePersistRemoteFunctions, isString, type ObservablePersistRemoteSaveParams } from '@legendapp/state';
 
-export function persistFetch(get: string | RequestInfo, set?: string | RequestInfo): ObservablePersistRemoteFunctions {
+interface PersistFetchProps {
+    get: string | RequestInfo;
+    set?: string | RequestInfo;
+    getInit?: RequestInit;
+    setInit?: RequestInit;
+    valueType?: 'arrayBuffer' | 'blob' | 'formData' | 'json' | 'text';
+}
+
+export function persistFetch({
+    get,
+    set,
+    getInit,
+    setInit,
+    valueType,
+}: PersistFetchProps): ObservablePersistRemoteFunctions {
     const ret: ObservablePersistRemoteFunctions = {
         get() {
-            return fetch(get);
+            return fetch(get, getInit).then((response) => response[valueType || 'json']());
         },
     };
 
     if (set) {
         ret.set = async ({ value }: ObservablePersistRemoteSaveParams<any>) => {
             const requestInfo = isString(set) ? ({ url: set } as RequestInfo) : set;
-            await fetch(Object.assign({ method: 'POST' }, requestInfo, { body: JSON.stringify(value) }));
+            await fetch(Object.assign({ method: 'POST' }, requestInfo, { body: JSON.stringify(value) }), setInit);
             // Return undefined to indicate no changes
         };
     }
