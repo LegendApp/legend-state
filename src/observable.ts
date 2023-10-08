8b403183bdc98725f63d1302f440b1e8bd639c42
Jay Meistrich <jmeistrich@gmail.com>
2023-10-08 17:05:30 +0200

    Refactor createObservable out into a different file so it can be used from ObservableObject without a circular reference error

--- src/observable.ts
+++ src/observable.ts
@@ -1,53 +1,16 @@
-import { extractPromise, getProxy } from './ObservableObject';
+import { createObservable } from './createObservable';
+import { getProxy } from './ObservableObject';
 import { ObservablePrimitiveClass } from './ObservablePrimitive';
-import { isActualPrimitive, isPromise } from './is';
-import type {
-    Observable,
-    ObservableObjectOrArray,
-    ObservablePrimitive,
-    ObservableRoot,
-    PromiseInfo,
-} from './observableInterfaces';
-import { NodeValue } from './observableInterfaces';
+import type { Observable, ObservablePrimitive, WithState } from './observableInterfaces';
 
-function createObservable<T>(value: Promise<T>, makePrimitive?: true): ObservablePrimitive<T & PromiseInfo>;
-function createObservable<T>(value?: T, makePrimitive?: true): ObservablePrimitive<T>;
-function createObservable<T>(
-    value?: Promise<T>,
-    makePrimitive?: boolean,
-): ObservablePrimitive<T & PromiseInfo> | ObservableObjectOrArray<T & PromiseInfo>;
-function createObservable<T>(value?: T, makePrimitive?: boolean): ObservablePrimitive<T> | ObservableObjectOrArray<T> {
-    const valueIsPromise = isPromise<T>(value);
-    const root: ObservableRoot = {
-        _: value,
-    };
-
-    const node: NodeValue = {
-        root,
-        lazy: true,
-    };
-
-    const prim = makePrimitive || isActualPrimitive(value);
-
-    const obs = prim
-        ? (new (ObservablePrimitiveClass as any)(node) as ObservablePrimitive<T>)
-        : (getProxy(node) as ObservableObjectOrArray<T>);
-
-    if (valueIsPromise) {
-        extractPromise(node, value);
-    }
-
-    return obs;
-}
-
-export function observable<T>(value: Promise<T>): Observable<T & PromiseInfo>;
+export function observable<T>(value: Promise<T>): Observable<T & WithState>;
 export function observable<T>(value?: T): Observable<T>;
-export function observable<T>(value?: T | Promise<T>): Observable<T & PromiseInfo> {
-    return createObservable(value) as Observable<T & PromiseInfo>;
+export function observable<T>(value?: T | Promise<T>): Observable<T & WithState> {
+    return createObservable(value, false, getProxy, ObservablePrimitiveClass) as Observable<T & WithState>;
 }
 
-export function observablePrimitive<T>(value: Promise<T>): ObservablePrimitive<T & PromiseInfo>;
+export function observablePrimitive<T>(value: Promise<T>): ObservablePrimitive<T & WithState>;
 export function observablePrimitive<T>(value?: T): ObservablePrimitive<T>;
-export function observablePrimitive<T>(value?: T | Promise<T>): ObservablePrimitive<T & PromiseInfo> {
-    return createObservable(value, /*makePrimitive*/ true) as ObservablePrimitive<T & PromiseInfo>;
+export function observablePrimitive<T>(value?: T | Promise<T>): ObservablePrimitive<T & WithState> {
+    return createObservable(value, true, getProxy, ObservablePrimitiveClass) as ObservablePrimitive<T & WithState>;
 }
