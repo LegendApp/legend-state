326c944b7a6f8abb97ba894c0ceec96d510d56e6
Jay Meistrich <jmeistrich@gmail.com>
2023-10-16 13:35:51 +0200

    improve some types

--- src/observable.ts
+++ src/observable.ts
@@ -10,14 +10,16 @@ import type {
 } from './observableInterfaces';
 
 export function observable<T>(value: Promise<T>): Observable<T & WithState>;
-export function observable<T>(value: (params: ComputedParams) => T): ObservableComputed<T>;
+export function observable<T>(value: (params: ComputedParams) => T): Observable<T>;
+export function observable<T>(value: () => T): ObservableComputed<T>;
 export function observable<T>(value?: T): Observable<T>;
 export function observable<T>(value?: T | Promise<T>): Observable<T & WithState> {
     return createObservable(value, false, getProxy, ObservablePrimitiveClass) as Observable<T & WithState>;
 }
 
 export function observablePrimitive<T>(value: Promise<T>): ObservablePrimitive<T & WithState>;
-export function observablePrimitive<T>(value: () => T): ObservableComputed<T>;
+export function observablePrimitive<T>(value: (params: ComputedParams) => T): ObservableComputed<T>;
+export function observablePrimitive<T>(value: () => T): ObservablePrimitive<T>;
 export function observablePrimitive<T>(value?: T): ObservablePrimitive<T>;
 export function observablePrimitive<T>(value?: T | Promise<T>): ObservablePrimitive<T & WithState> {
     return createObservable(value, true, getProxy, ObservablePrimitiveClass) as ObservablePrimitive<T & WithState>;
