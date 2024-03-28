506ab8d30cdc7a0986be82ab2bd73302fbd91e09
Jay Meistrich <jmeistrich@gmail.com>
2024-03-27 17:41:12 -0700

    remove trackSelector optimization for selector being an observable because it skipped all the normal tracking, and wasn't a big optimization anyway

--- src/trackSelector.ts
+++ src/trackSelector.ts
@@ -1,12 +1,5 @@
-import { computeSelector, isObservable } from './helpers';
-import type {
-    ListenerParams,
-    NodeValue,
-    ObservableListenerDispose,
-    ObserveEvent,
-    Selector,
-    TrackingNode,
-} from './observableInterfaces';
+import { computeSelector } from './helpers';
+import type { ListenerParams, ObserveEvent, Selector } from './observableInterfaces';
 import type { ObserveOptions } from './observe';
 import { setupTracking } from './setupTracking';
 import { beginTracking, endTracking, tracking } from './tracking';
@@ -16,36 +9,25 @@ export function trackSelector<T>(
     update: (params: ListenerParams) => void,
     observeEvent?: ObserveEvent<T>,
     observeOptions?: ObserveOptions,
-    createResubscribe?: boolean,
 ) {
-    let nodes: Map<NodeValue, TrackingNode> | undefined;
-    let value;
-    let dispose;
-    let tracker;
-    let resubscribe: ObservableListenerDispose | undefined;
+    let dispose: (() => void) | undefined = undefined;
     let updateFn = update;
 
-    if (isObservable(selector)) {
-        value = selector.peek();
-        dispose = selector.onChange(update);
-        resubscribe = createResubscribe ? selector.onChange(update) : undefined;
-    } else {
-        // Compute the selector inside a tracking context
-        beginTracking();
-        value = selector ? computeSelector(selector, observeEvent, observeOptions?.fromComputed) : selector;
-        tracker = tracking.current;
-        nodes = tracker!.nodes;
-        endTracking();
+    // Compute the selector inside a tracking context
+    beginTracking();
+    const value = selector ? computeSelector(selector, observeEvent, observeOptions?.fromComputed) : selector;
+    const tracker = tracking.current;
+    const nodes = tracker!.nodes;
+    endTracking();
 
-        if ((process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') && tracker && nodes) {
-            tracker.traceListeners?.(nodes);
-            if (tracker.traceUpdates) {
-                updateFn = tracker.traceUpdates(update) as () => void;
-            }
-            // Clear tracing so it doesn't leak to other components
-            tracker.traceListeners = undefined;
-            tracker.traceUpdates = undefined;
+    if ((process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') && tracker && nodes) {
+        tracker.traceListeners?.(nodes);
+        if (tracker.traceUpdates) {
+            updateFn = tracker.traceUpdates(update) as () => void;
         }
+        // Clear tracing so it doesn't leak to other components
+        tracker.traceListeners = undefined;
+        tracker.traceUpdates = undefined;
     }
 
     if (!observeEvent?.cancel) {
@@ -54,8 +36,7 @@ export function trackSelector<T>(
         // useSyncExternalStore doesn't subscribe until after the component mount.
         // We want to subscribe immediately so we don't miss any updates
         dispose = setupTracking(nodes, updateFn, false, observeOptions?.immediate);
-        resubscribe = createResubscribe ? () => setupTracking(nodes, updateFn) : undefined;
     }
 
-    return { value, dispose, resubscribe };
+    return { value, dispose };
 }
