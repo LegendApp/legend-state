9f40045229271570ae4dbf8adfdfe0c714fa7583
Jay Meistrich <jmeistrich@gmail.com>
2024-05-11 21:04:23 -0700

    change transform tests to not use a promiseTimeout to work better in bun tests

--- tests/transform.test.ts
+++ tests/transform.test.ts
@@ -7,7 +7,6 @@ import {
     transformStringifyKeys,
 } from '@legendapp/state/sync';
 import { expectTypeOf } from 'expect-type';
-import { promiseTimeout } from './testglobals';
 
 describe('transform', () => {
     test('TransformStringsToDates', async () => {
@@ -25,7 +24,7 @@ describe('transform', () => {
         expectTypeOf<Local['num']>().toEqualTypeOf<number>();
     });
     test('transform dates args', async () => {
-        let setVal = undefined;
+        const setVal$ = observable();
         const date = new Date();
         interface Remote {
             text: string | null;
@@ -39,7 +38,7 @@ describe('transform', () => {
                         date: date.toISOString(),
                     }) as Remote,
                 set: ({ value }) => {
-                    setVal = value;
+                    setVal$.set(value);
                 },
                 transform: transformStringifyDates<Remote, 'date'>('date'),
             }),
@@ -55,13 +54,13 @@ describe('transform', () => {
         const dateNew = new Date(+date + 1000);
         obs$.date.set(dateNew);
 
-        await promiseTimeout(0);
+        const setVal = await when(setVal$);
 
         expect(setVal).toEqual({ text: 'hi', date: dateNew.toISOString() });
         expect(obs$.get()).toEqual({ text: 'hi', date: dateNew });
     });
     test('transform dates auto', async () => {
-        let setVal = undefined;
+        const setVal$ = observable();
         const date = new Date();
         interface Remote {
             text: string | null;
@@ -78,7 +77,7 @@ describe('transform', () => {
                         date: date.toISOString(),
                     }) as Remote,
                 set: ({ value }) => {
-                    setVal = value;
+                    setVal$.set(value);
                 },
                 transform: transformStringifyDates<Remote, Local>(),
             }),
@@ -94,13 +93,13 @@ describe('transform', () => {
         const dateNew = new Date(+date + 1000);
         obs$.date.set(dateNew);
 
-        await promiseTimeout(0);
+        const setVal = await when(setVal$);
 
         expect(setVal).toEqual({ text: 'hi', date: dateNew.toISOString() });
         expect(obs$.get()).toEqual({ text: 'hi', date: dateNew });
     });
     test('transform keys', async () => {
-        let setVal = undefined;
+        const setVal$ = observable();
         interface Remote {
             text: string | null;
             num: string | null;
@@ -123,7 +122,7 @@ describe('transform', () => {
                         obj: JSON.stringify({ key: 'value' }),
                     }) as Remote,
                 set: ({ value }) => {
-                    setVal = value;
+                    setVal$.set(value);
                 },
                 transform: transformStringifyKeys<Remote, Local>('arr', 'num', 'obj'),
             }),
@@ -135,7 +134,7 @@ describe('transform', () => {
 
         obs$.arr.push(4);
 
-        await promiseTimeout(0);
+        const setVal = await when(setVal$);
 
         expect(setVal).toEqual({
             text: 'hi',
@@ -146,7 +145,7 @@ describe('transform', () => {
         expect(obs$.get()).toEqual({ text: 'hi', arr: [1, 2, 3, 4], num: 10, obj: { key: 'value' } });
     });
     test('transform everything', async () => {
-        let setVal = undefined;
+        const setVal$ = observable();
         const date = new Date();
         interface Remote {
             text: string | null;
@@ -173,7 +172,7 @@ describe('transform', () => {
                         obj: JSON.stringify({ key: 'value' }),
                     }) as Remote,
                 set: ({ value }) => {
-                    setVal = value;
+                    setVal$.set(value);
                 },
                 transform: combineTransforms(
                     transformStringifyDates<Remote, Local>(),
@@ -190,7 +189,7 @@ describe('transform', () => {
         obs$.date.set(dateNew);
         obs$.arr.push(4);
 
-        await promiseTimeout(0);
+        const setVal = await when(setVal$);
 
         expect(setVal).toEqual({
             text: 'hi',
