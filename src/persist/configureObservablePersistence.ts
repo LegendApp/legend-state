ddfc9106379b7eaa99a7dfffad9978184eb63fab
Jay Meistrich <jmeistrich@gmail.com>
2024-06-11 12:59:38 -0700

    remove last of old persist plugins

--- src/persist/configureObservablePersistence.ts
+++
@@ -1,7 +0,0 @@
-import type { ObservablePersistenceConfig } from '@legendapp/state/sync';
-
-export const observablePersistConfiguration: ObservablePersistenceConfig = {};
-
-export function configureObservablePersistence(options?: ObservablePersistenceConfig) {
-    Object.assign(observablePersistConfiguration, options);
-}
