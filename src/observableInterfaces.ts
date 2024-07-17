3bdb6b5945e270f009ae7e5d417003e51e7a7f25
Jay Meistrich <jmeistrich@gmail.com>
2024-07-16 17:08:55 -0700

    a comment

--- src/observableInterfaces.ts
+++ src/observableInterfaces.ts
@@ -44,6 +44,7 @@ export type ClassConstructor<I, Args extends any[] = any[]> = new (...args: Args
 export type ObservableListenerDispose = () => void;
 
 export interface ObservableRoot {
+    // Observable root value is set on a child of the object so the reference to the root never changes
     _: any;
     set?: (value: any) => void;
 }
