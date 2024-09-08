afb6534cc595491f9b228b75741413684bfe55d9
Jay Meistrich <jmeistrich@gmail.com>
2024-09-08 17:09:55 +0200

    fix types of configureSynced

--- src/sync/configureSynced.ts
+++ src/sync/configureSynced.ts
@@ -1,18 +1,17 @@
 import { internal } from '@legendapp/state';
-import type { PersistOptions, SyncedOptions } from './syncTypes';
+import type { SyncedOptions } from './syncTypes';
 import { synced } from './synced';
 
 const { deepMerge } = internal;
 
-interface SyncedOptionsConfigure extends Omit<SyncedOptions, 'persist'> {
-    persist?: Partial<PersistOptions<any>>;
-}
-
-export function configureSynced<T extends (...args: any[]) => any>(fn: T, origOptions: Parameters<T>[0]): T;
-export function configureSynced(origOptions: SyncedOptions): (option: SyncedOptions) => SyncedOptions;
-export function configureSynced<T extends (...args: any[]) => any>(fnOrOrigOptions: T, origOptions?: Parameters<T>[0]) {
+export function configureSynced<T extends (...args: any[]) => any>(fn: T, origOptions: Partial<Parameters<T>[0]>): T;
+export function configureSynced(origOptions: SyncedOptions): typeof synced;
+export function configureSynced<T extends (...args: any[]) => any>(
+    fnOrOrigOptions: T,
+    origOptions?: Partial<Parameters<T>[0]>,
+) {
     const fn = origOptions ? (fnOrOrigOptions as T) : synced;
-    origOptions = origOptions ?? (fnOrOrigOptions as SyncedOptionsConfigure);
+    origOptions = origOptions ?? fnOrOrigOptions;
 
     return (options: SyncedOptions) => {
         const merged = deepMerge(origOptions as any, options);
