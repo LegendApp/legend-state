0f1bda33cbdf6566836a05bb2439817e60a02882
Jay Meistrich <jmeistrich@gmail.com>
2024-09-16 16:01:30 +0200

    react query plugin gets queryClient from context if not included in params

--- src/sync-plugins/tanstack-react-query.ts
+++ src/sync-plugins/tanstack-react-query.ts
@@ -1,6 +1,7 @@
 import { useObservable } from '@legendapp/state/react';
 import { SyncedQueryParams, syncedQuery } from '@legendapp/state/sync-plugins/tanstack-query';
 import { DefaultError, QueryKey } from '@tanstack/query-core';
+import { useQueryClient } from '@tanstack/react-query';
 import type { Observable } from '@legendapp/state';
 import type { Synced } from '@legendapp/state/sync';
 
@@ -10,5 +11,12 @@ export function useObservableSyncedQuery<
     TData = TQueryFnData,
     TQueryKey extends QueryKey = QueryKey,
 >(params: SyncedQueryParams<TQueryFnData, TError, TData, TQueryKey>): Observable<Synced<TData>> {
-    return useObservable(syncedQuery(params));
+    const queryClient = params.queryClient || useQueryClient();
+
+    return useObservable(
+        syncedQuery({
+            ...params,
+            queryClient,
+        }),
+    );
 }
