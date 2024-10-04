13ddf5140d57d6d485956bd4ce83d7dd2335aad4
Jay Meistrich <jmeistrich@gmail.com>
2024-10-05 00:16:36 +0200

    Update types of useWhen to return a promise

--- src/react/useWhen.ts
+++ src/react/useWhen.ts
@@ -1,9 +1,13 @@
 import { Selector, when, whenReady } from '@legendapp/state';
 import { useMemo } from 'react';
 
-export function useWhen<T>(predicate: Selector<T>, effect: (value: T) => any | (() => any)) {
-    return useMemo(() => when(predicate, effect), []);
+export function useWhen<T>(predicate: Selector<T>): Promise<T>;
+export function useWhen<T, T2>(predicate: Selector<T>, effect: (value: T) => T2): Promise<T2>;
+export function useWhen<T, T2>(predicate: Selector<T>, effect?: (value: T) => T2): Promise<T2> {
+    return useMemo(() => when<T, T2>(predicate, effect as any), []);
 }
-export function useWhenReady<T>(predicate: Selector<T>, effect: (value: T) => any | (() => any)) {
-    return useMemo(() => whenReady(predicate, effect), []);
+export function useWhenReady<T>(predicate: Selector<T>): Promise<T>;
+export function useWhenReady<T, T2>(predicate: Selector<T>, effect: (value: T) => T2 | (() => T2)): Promise<T2>;
+export function useWhenReady<T, T2>(predicate: Selector<T>, effect?: (value: T) => T2 | (() => T2)): Promise<T2> {
+    return useMemo(() => whenReady<T, T2>(predicate, effect as any), []);
 }
