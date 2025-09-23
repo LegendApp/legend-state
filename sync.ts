64f26ded228c4491670c81de50f3193af2b625a8
Jay Meistrich <jmeistrich@gmail.com>
2025-09-23 23:30:05 +0200

    feat: export getAllSyncStates

--- sync.ts
+++ sync.ts
@@ -2,7 +2,7 @@ import type { SyncedOptionsGlobal } from './src/sync/syncTypes';
 export { configureObservableSync } from './src/sync/configureObservableSync';
 export * from './src/sync/persistTypes';
 export * from './src/sync/syncHelpers';
-export { mapSyncPlugins, onChangeRemote, syncObservable } from './src/sync/syncObservable';
+export { mapSyncPlugins, onChangeRemote, syncObservable, getAllSyncStates } from './src/sync/syncObservable';
 export * from './src/sync/syncTypes';
 export { synced } from './src/sync/synced';
 export * from './src/sync/configureSynced';
